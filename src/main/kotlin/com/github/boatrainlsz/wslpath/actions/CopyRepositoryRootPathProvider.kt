61a8f81baff908ac44f43a39e1c7b099ae729854
boatrainlsz <yinger521999@gmail.com>
2022-10-28 16:53:34 +0800

    fix:remove redundant semicolon

--- src/main/kotlin/com/github/boatrainlsz/wslpath/actions/CopyRepositoryRootPathProvider.kt
+++ src/main/kotlin/com/github/boatrainlsz/wslpath/actions/CopyRepositoryRootPathProvider.kt
@@ -8,7 +8,7 @@ import com.intellij.openapi.vfs.VirtualFile
 class CopyPathInWSLProvider : DumbAwareCopyPathProvider() {
     override fun getPathToElement(project: Project, virtualFile: VirtualFile?, editor: Editor?): String? {
         if (virtualFile == null) return null
-        val presentableUrl = virtualFile.presentableUrl;
+        val presentableUrl = virtualFile.presentableUrl
         //presentableUrl is like: D:\research\wslpath\filename.txt
         //we want to convert it to /mnt/d/research/wslpath/filename.txt, D is upper case, need to convert to lower case
         val path = presentableUrl.substring(0, 2).lowercase() + presentableUrl.substring(2)
