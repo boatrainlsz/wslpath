7975251f41b9ff52e9d092c0e76cec816389a872
boatrainlsz <yinger521999@gmail.com>
2022-10-28 14:49:07 +0800

    feature:wslpath almost done

--- src/main/kotlin/com/github/boatrainlsz/wslpath/actions/CopyRepositoryRootPathProvider.kt
+++ src/main/kotlin/com/github/boatrainlsz/wslpath/actions/CopyRepositoryRootPathProvider.kt
@@ -3,17 +3,15 @@ package com.github.boatrainlsz.wslpath.actions
 import com.intellij.ide.actions.DumbAwareCopyPathProvider
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.ProjectLevelVcsManager
-import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 
 class CopyPathInWSLProvider : DumbAwareCopyPathProvider() {
-  override fun getPathToElement(project: Project, virtualFile: VirtualFile?, editor: Editor?): String? {
-    if (virtualFile == null) return null
-
-    val vcsRoot = ProjectLevelVcsManager.getInstance(project).getVcsRootObjectFor(virtualFile)
-    if (vcsRoot == null) return null
-
-    return VfsUtilCore.getRelativePath(virtualFile, vcsRoot.path)
-  }
+    override fun getPathToElement(project: Project, virtualFile: VirtualFile?, editor: Editor?): String? {
+        if (virtualFile == null) return null
+        val presentableUrl = virtualFile.presentableUrl;
+        //presentableUrl is like: D:\research\wslpath\filename.txt
+        //we want to convert it to /mnt/d/research/wslpath/filename.txt, D is upper case, need to convert to lower case
+        val path = presentableUrl.substring(0, 2).lowercase() + presentableUrl.substring(2)
+        return "/mnt/" + path.replace("\\", "/").replace(":", "")
+    }
 }
